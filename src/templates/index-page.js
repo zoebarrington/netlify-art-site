769ebdb71a6cddf8383ade700a6f19392b6eb3e1
Zoe Barrington <zoe.barrington@infinityworks.com>
2023-11-13 17:58:19 +0000

    removed most elements from homepage and added heading

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -22,7 +22,7 @@ export const IndexPageTemplate = ({
 
   return (
     <div>
-      <FullWidthImage img={heroImage} title={title} subheading={subheading} />
+      {/* <FullWidthImage img={heroImage} title={title} subheading={subheading} /> */}
       <section className="section section--gradient">
         <div className="container">
           <div className="section">
@@ -33,27 +33,27 @@ export const IndexPageTemplate = ({
                     <div className="tile">
                       <h1 className="title">{mainpitch.title}</h1>
                     </div>
-                    <div className="tile">
-                      <h3 className="subtitle">{mainpitch.description}</h3>
-                    </div>
+                    {/* <div className="tile"> */}
+                      {/* <h3 className="subtitle">{mainpitch.description}</h3> */}
+                    {/* </div> */}
                   </div>
-                  <div className="columns">
+                  {/* <div className="columns">
                     <div className="column is-12">
                       <h3 className="has-text-weight-semibold is-size-2">
                         {heading}
                       </h3>
                       <p>{description}</p>
                     </div>
-                  </div>
-                  <Features gridItems={intro.blurbs} />
-                  <div className="columns">
+                  </div> */}
+                  {/* <Features gridItems={intro.blurbs} /> */}
+                  {/* <div className="columns">
                     <div className="column is-12 has-text-centered">
                       <Link className="btn" to="/products">
                         See all products
                       </Link>
                     </div>
-                  </div>
-                  <div className="column is-12">
+                  </div> */}
+                  {/* <div className="column is-12">
                     <h3 className="has-text-weight-semibold is-size-2">
                       Latest stories
                     </h3>
@@ -63,7 +63,7 @@ export const IndexPageTemplate = ({
                         Read more
                       </Link>
                     </div>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             </div>
