e9971b81d173be7fa10fc3384098d3f9f60cc9b0
Zoe Barrington <zoe.barrington@infinityworks.com>
2023-12-13 16:58:12 +0000

    deleting blogs to start again

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,7 +8,7 @@ import { withPrefix } from "gatsby";
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata();
   return (
-    <div>
+    <div className="full-page-container">
       <Helmet>
         <html lang="en" />
         <title>{title}</title>
