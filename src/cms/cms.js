42fc58ffcb5547f1824f3925d15012c6b385446c
Zoe Barrington <zoebarrington@Zoes-MacBook-Pro.local>
2024-10-20 22:20:13 +0100

    test

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,11 +4,11 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
+// import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('index', IndexPagePreview)
+// CMS.registerPreviewTemplate('index', IndexPagePreview)
