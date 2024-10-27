bfbe0d1bf662368601736632e528d0dad43deff7
Zoe Barrington <zoebarrington@Zoes-MacBook-Pro.local>
2024-10-27 19:26:35 +0000

    testing

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types'
 import { BlogPostTemplate } from '../../templates/blog-post'
 
 const BlogPostPreview = ({ entry, widgetFor }) => {
+  if (!entry) {
+    console.log('Entry is undefined'); // This will help to identify if entry is not passed
+    return null; // Or render a loading state if needed
+  }
   console.log(entry, "entry"); // Add this line to inspect the entry object
 
   return (
